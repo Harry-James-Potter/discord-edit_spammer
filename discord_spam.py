e2e656f67b6eed3316a74f9c9d6ea54103e74f7d
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-01-20 15:50:51 -0500

    Add files via upload

---
+++ discord_spam.py
@@ -0,0 +1,19 @@
+import clipboard, time
+from pynput import mouse, keyboard
+mousee = mouse.Controller()
+keyboardd = keyboard.Controller()
+time.sleep(10) # GOTTA GO TO APP
+le_num = 1
+while True: # LOOP DE LOOP
+    if not le_num == 1:
+        keyboardd.press(keyboard.Key.up)
+        keyboardd.release(keyboard.Key.enter)
+        keyboardd.press(keyboard.Key.space)
+        keyboardd.release(keyboard.Key.space)
+    keyboardd.type(str(le_num))
+    keyboardd.press(keyboard.Key.enter)
+    keyboardd.release(keyboard.Key.enter)
+    time.sleep(1.5) # ADJUST ACCORGINGLY
+    le_num += 1
+    if le_num == 101: # TILL.....
+        break
